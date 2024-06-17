bdcb55a3cd550e3c7921a26082fc03abef1bfc2b
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-17 17:03:06 +0200

    add aggiunta lunghezza paragrafo

--- contact_page.php
+++ contact_page.php
@@ -1,10 +1,27 @@
 <?php 
+// imposto le variabili
 $name = $_GET["name"];
 $last_name = $_GET["last-name"];
 $email = $_GET["email"];
 $password = $_GET["password"];
 
-echo $name;
-echo $last_name;
-echo $email;
+// calcolo lunghezza delle variabili
+$name_length = strlen($name);
+$last_name_length = strlen($last_name);
+$email_length = strlen($email);
+$password_length = strlen($password);
+
+
+// stampo in pagina il risultato
+echo "<p>Il nome è: $name (Lunghezza: $name_length)</p>";
+echo "<p> Il cognome è: $last_name (Lunghezza: $last_name_length)</p>";
+echo "<p> l'indirizzo email è:$email (Lunghezza: $email_length)</p>";
+echo "<p>la password è: $password (Lunghezza: $password_length)</p>";
+
+// stampo in pagina il risultato censurato
+echo "<h2>Censura applicata</h2>";
+echo "<p>Il nome è: $name (Lunghezza: $name_length)</p>";
+echo "<p> Il cognome è: $last_name (Lunghezza: $last_name_length)</p>";
+echo "<p> l'indirizzo email è:$email (Lunghezza: $email_length)</p>";
+echo "<p>la password è: $password (Lunghezza: $password_length)</p>";
 ?>
