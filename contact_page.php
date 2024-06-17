ec88dae4a178b62c3c73394fd0d9cf7cff90c2e6
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-06-17 17:26:01 +0200

    add ora la password viene censurata

--- contact_page.php
+++ contact_page.php
@@ -11,6 +11,9 @@ $last_name_length = strlen($last_name);
 $email_length = strlen($email);
 $password_length = strlen($password);
 
+// sostituisco la parola da censurare 
+$parola_censurata = str_replace($password,'***',$password);
+
 
 // stampo in pagina il risultato
 echo "<p>Il nome è: $name (Lunghezza: $name_length)</p>";
@@ -23,5 +26,5 @@ echo "<h2>Censura applicata</h2>";
 echo "<p>Il nome è: $name (Lunghezza: $name_length)</p>";
 echo "<p> Il cognome è: $last_name (Lunghezza: $last_name_length)</p>";
 echo "<p> l'indirizzo email è:$email (Lunghezza: $email_length)</p>";
-echo "<p>la password è: $password (Lunghezza: $password_length)</p>";
+echo "<p>la password è: $parola_censurata (Lunghezza: $password_length)</p>";
 ?>
